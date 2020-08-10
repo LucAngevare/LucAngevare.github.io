5383f4fdad92047b17ac40806969a7783900ceef
Luc <lucangevare@gmail.com>
2020-08-10 22:36:41 +0200

    Added the search for projects
    I am working on an API to host the JSON name/links so it's easier for me to update the autocorrect function but for now this'll do.

---
+++ assets/js/myStuff.js
@@ -0,0 +1,201 @@
+var jsonData = [{
+  link: "https://repl.it/@S0S/generalBotForServer",
+  name: "generalBotForServer"
+}, {
+  link: "https://repl.it/@S0S/WebScraper",
+  name: "WebScraper"
+}, {
+  link: "https://repl.it/@S0S/AESencryption",
+  name: "AESencryption"
+}, {
+  link: "https://repl.it/@S0S/SortingAlgorithms",
+  name: "SortingAlgorithms"
+}, {
+  link: "https://repl.it/@S0S/colorPaletteGenerator",
+  name: "colorPaletteGenerator"
+}, {
+  link: "https://repl.it/@S0S/ApplicationBot",
+  name: "ApplicationBot"
+}, {
+  link: "https://repl.it/@S0S/ViginereCipher",
+  name: "ViginereCipher"
+}, {
+  link: "https://repl.it/@S0S/MessengerSender",
+  name: "MessengerSender"
+}, {
+  link: "https://repl.it/@S0S/MovieShit",
+  name: "MovieShit"
+}, {
+  link: "https://repl.it/@S0S/HamiltonianCycle",
+  name: "HamiltonianCycle"
+}, {
+  link: "https://repl.it/@S0S/HomeServer",
+  name: "HomeServer"
+}, {
+  link: "https://repl.it/@S0S/VoteBot",
+  name: "VoteBot"
+}, {
+  link: "https://repl.it/@S0S/OffenseBot",
+  name: "OffenseBot"
+}, {
+  link: "https://repl.it/@S0S/Pathfinder",
+  name: "Pathfinder"
+}, {
+  link: "https://repl.it/@S0S/DecryptionAlgorithm",
+  name: "DecryptionAlgorithm"
+}, {
+  link: "https://repl.it/@S0S/EpsilonTelegramIoT",
+  name: "EpsilonTelegramIoT"
+}, {
+  link: "https://repl.it/@S0S/Local-Music-Player",
+  name: "Local-Music-Player"
+}, {
+  link: "https://repl.it/@S0S/Emotion-from-text",
+  name: "Emotion-from-text"
+}, {
+  link: "https://repl.it/@S0S/ELITE",
+  name: "ELITE"
+}, {
+  link: "https://repl.it/@S0S/CrashScript",
+  name: "CrashScript"
+}, {
+  link: "https://repl.it/@S0S/Sir",
+  name: "Sir"
+}, {
+  link: "https://repl.it/@S0S/Upcoming-Event-Calendar",
+  name: "Upcoming-Event-Calendar"
+}, {
+  link: "https://repl.it/@S0S/5-day-forecast-with-icons",
+  name: "5-day-forecast-with-icons"
+}, {
+  link: "https://repl.it/@S0S/Tkinter-Thing",
+  name: "Tkinter-Thing"
+}, {
+  link: "https://repl.it/@S0S/OddlySatisfying",
+  name: "OddlySatisfying"
+}, {
+  link: "https://repl.it/@S0S/Decryption",
+  name: "Decryption"
+}, {
+  link: "https://repl.it/@S0S/YouTube-Python",
+  name: "YouTube-Python"
+}, {
+  link: "https://repl.it/@S0S/Encryptor",
+  name: "Encryptor"
+}, {
+  link: "https://repl.it/@S0S/ImgSearch",
+  name: "ImgSearch"
+}, {
+  link: "https://repl.it/@S0S/GA",
+  name: "GA"
+}, {
+  link: "https://repl.it/@S0S/Tic-Tac-Toe",
+  name: "Tic-Tac-Toe"
+}, {
+  link: "https://repl.it/@LucAngevare/HuffmanEncryption",
+  name: "HuffmanEncryption"
+}, {
+  link: "https://repl.it/@LucAngevare/TechSupportTicketBot",
+  name: "TechSupportTicketBot"
+}, {
+  link: "https://repl.it/@LucAngevare/startTTSengine",
+  name: "startTTSengine"
+}, {
+  link: "https://repl.it/@LucAngevare/APIlistener",
+  name: "APIlistener"
+}, {
+  link: "https://repl.it/@LucAngevare/searchThroughDB",
+  name: "searchThroughDB"
+}, {
+  link: "https://repl.it/@LucAngevare/recursiveSearch",
+  name: "recursiveSearch"
+}, {
+  link: "https://repl.it/@LucAngevare/ToDoApp",
+  name: "ToDoApp"
+}, {
+  link: "https://repl.it/@LucAngevare/generalBotForServer",
+  name: "generalBotForServer"
+}, {
+  link: "https://repl.it/@LucAngevare/swearFilter",
+  name: "swearFilter"
+}, {
+  link: "https://repl.it/@LucAngevare/NLP",
+  name: "NLP"
+}, {
+  link: "https://repl.it/@LucAngevare/VoedingOpdracht4",
+  name: "VoedingOpdracht4"
+}, {
+  link: "https://repl.it/@LucAngevare/CopyEmojis",
+  name: "CopyEmojis"
+}, {
+  link: "https://repl.it/@LucAngevare/NeuralNetwork",
+  name: "NeuralNetwork"
+}, {
+  link: "https://repl.it/@LucAngevare/HamiltonianCycle",
+  name: "HamiltonianCycle"
+}, {
+  link: "https://repl.it/@LucAngevare/A-Python",
+  name: "A-Python"
+}, {
+  link: "https://github.com/LucAngevare/LucAngevare",
+  name: "LucAngevare"
+}, {
+  link: "https://github.com/LucAngevare/LucAngevare.github.io",
+  name: "LucAngevare.github.io"
+}, {
+  link: "https://github.com/LucAngevare/API",
+  name: "API"
+}, {
+  link: "https://github.com/LucAngevare/FaceDetect",
+  name: "FaceDetect"
+}, {
+  link: "https://github.com/LucAngevare/VoteBot",
+  name: "VoteBot"
+}, {
+  link: "https://github.com/LucAngevare/HelperBot",
+  name: "HelperBot"
+}, {
+  link: "https://github.com/LucAngevare/SnakeGeneticAlgorithm",
+  name: "SnakeGeneticAlgorithm"
+}, {
+  link: "https://github.com/LucAngevare/OffenseBot",
+  name: "OffenseBot"
+}, {
+  link: "https://github.com/LucAngevare/OddlySatisfying",
+  name: "OddlySatisfying"
+}, {
+  link: "https://github.com/LucAngevare/Ludle",
+  name: "Ludle"
+}, {
+  link: "https://github.com/LucAngevare/WeatherApp",
+  name: "WeatherApp"
+}, {
+  link: "https://github.com/LucAngevare/RandomizerAndroidApp",
+  name: "RandomizerAndroidApp"
+}, {
+  link: "https://github.com/LucAngevare/OwO-Translator",
+  name: "OwO-Translator"
+}]
+
+$('#jQuerySearchThing').autocomplete({
+    source: function (request, response) {
+      response($.map(jsonData, function (obj, key) {
+
+        var name = obj.name.toUpperCase();
+
+        if (name.indexOf(request.term.toUpperCase()) != -1) {
+          return {
+            label: obj.name,
+            value: obj.link
+          }
+        } else {
+          return null;
+        }
+      }));
+    },
+    // Once a value in the drop down list is selected, do the following:
+    select: function(event, ui) {
+      $('#jQuerySearchThing').val(ui.item.label);
+      window.open(ui.item.value)
+    }
+  })
